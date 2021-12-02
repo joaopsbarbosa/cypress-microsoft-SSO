b394d1f4fbab6faf37b3a097e1c5349f590b02ec
João Barbosa <joaobarbosa.ic@gmail.com>
2021-12-02 14:24:00 +0000

    Update example.js

--- cypress/integration/example.js
+++ cypress/integration/example.js
@@ -6,7 +6,7 @@
 describe('Open Microsoft using SSO', () => {
 
     it('SSO on Microsoft', () => {
-        cy.Microsoft_SSO('https://login.microsoftonline.com/', 'YOUR_EMAIL_ACCOUNT_HERE', 'YOUR_PASSWORD_HERE')
+        cy.microsoftSSO('https://login.microsoftonline.com/', 'YOUR_EMAIL_ACCOUNT_HERE', 'YOUR_PASSWORD_HERE')
   
     })
 
